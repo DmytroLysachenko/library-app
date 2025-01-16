0ebad9d03221a4cadf7aa87f057ecdd874ca83db
DmytroLysachenko <dlysachenko98@gmail.com>
2025-01-16 19:29:57 +0100

    Added styles, fonts, home page created

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import React from "react";
+
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <main className="root-container">
+      <div className="mx-auto max-w-7xl">
+        <Header />
+        <div className="mt-20 pb-20">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
