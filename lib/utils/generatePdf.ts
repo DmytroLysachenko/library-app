017159f9b3e0757536af812619b527aa019c4c81
DmytroLysachenko <dlysachenko98@gmail.com>
2025-02-06 10:41:30 +0100

    Removed console logs from generatePdf file

--- lib/utils/generatePdf.ts
+++ lib/utils/generatePdf.ts
@@ -19,21 +19,15 @@ export const generatePdf = async (data: {
   websiteUrl: string;
   supportEmail: string;
 }) => {
-  console.log("Launching Puppeteer...");
-
   const browser = await puppeteer.launch({
     args: chromium.args,
     executablePath: await chromium.executablePath(),
   });
 
-  console.log("Puppeteer launched successfully.");
-
   const page = await browser.newPage();
   await page.setContent(emailTemplates.RECEIPT(data), {
     waitUntil: "networkidle0",
   });
-
-  console.log("Generating PDF...");
 
   const pdfBytes = await page.pdf({
     format: "A4",
@@ -43,8 +37,6 @@ export const generatePdf = async (data: {
 
   await browser.close();
 
-  console.log("PDF generated. Uploading to S3...");
-
   const fileName = `receipts/receipt_${Date.now()}.pdf`;
 
   const pdfUrl = await uploadToS3(
@@ -52,8 +44,6 @@ export const generatePdf = async (data: {
     fileName,
     "application/pdf"
   );
-
-  console.log("PDF uploaded to S3:", pdfUrl);
 
   return pdfUrl;
 };
